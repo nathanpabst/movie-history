4890a068935303375185ba35ffe24fc9b74943ed
Nathan Pabst <nathan.pabst@gmail.com>
2018-05-22 20:39:44 -0500

    wip

---
+++ movie-history/javascripts/apiKeys.js
@@ -0,0 +1,25 @@
+const tmdb = require('./tmdb');
+
+const apiKeys = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax('./db/apiKeys.json')
+      .done((data) => {
+        resolve(data.apiKeys);
+      })
+      .fail((err) => {
+        reject(err);
+      });
+  });
+};
+
+const retrieveKeys = () => {
+  apiKeys()
+    .then((results) => {
+      tmdb.setKey(results.tmdb.apiKey);
+    })
+    .catch((err) => {
+      console.error('no keys:', err);
+    });
+};
+
+module.exports = retrieveKeys;
