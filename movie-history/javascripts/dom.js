c9efbf6104bda500e4a13e7b02e1ecf610eb775a
Nathan Pabst <nathan.pabst@gmail.com>
2018-05-22 20:23:42 -0500

    added api key example

--- movie-history/javascripts/dom.js
+++ movie-history/javascripts/dom.js
@@ -2,14 +2,14 @@ const domString = (movieArray) => {
   let strang = '';
   movieArray.forEach((movie) => {
     strang += `<div class="col-sm-6 col-md-4">`;
-    strang +=  `<div class="thumbnail">`;
-    strang +=  `<img src="..." alt="...">`;
-    strang +=  `<div class="caption">`;
-    strang +=  `<h3>${movie.original_title}</h3>`;
-    strang +=  `<p>${movie.overview}</p>`;
-    strang +=  `<p><a href="#" class="btn btn-primary" role="button">Review</a> <a href="#" class="btn btn-default" role="button">Wishlist</a></p>`;
-    strang +=  `</div>`;
-    strang +=  `</div>`;
+    strang +=   `<div class="thumbnail">`;
+    strang +=     `<img src="..." alt="...">`;
+    strang +=   `<div class="caption">`;
+    strang +=     `<h3>${movie.original_title}</h3>`;
+    strang +=      `<p>${movie.overview}</p>`;
+    strang +=       `<p><a href="#" class="btn btn-primary" role="button">Review</a> <a href="#" class="btn btn-default" role="button">Wishlist</a></p>`;
+    strang +=     `</div>`;
+    strang +=   `</div>`;
     strang +=  `</div>`;
   });
 
